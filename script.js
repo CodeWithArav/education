d53e1e335c228d0625f15f82c8366315e9fd763f
CodeWithArav <121603770+CodeWithArav@users.noreply.github.com>
2024-03-18 00:37:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ let scroll_to_top_btn = document.querySelector('.scroll-up-btn');
 let small_screen_nav_bar_btn = document.querySelector('.small-screen-nav-bar-btn');
 let small_screen_nav_bar_links_container = document.querySelector('.small-screen-nav-bar-links-container');
 let small_screen_nav_links = document.querySelectorAll('.small-screen-nav-links');
+let theme_change_btn = document.getElementById('theme_change_btn');
 
-// ---------- Nav-Bar InnerHTML ---------- //
+let light_theme_text = document.querySelectorAll('.light-theme-text');
+let light_theme_indicator = document.querySelector('.light_theme_indicator');
+let light_theme_background = document.querySelectorAll('.light-theme-background');
+let light_theme_primary = document.querySelectorAll('.light-theme-primary');
 
-// nav_bar.innerHTML = `
+let light_theme_form_fields = document.querySelectorAll('.light-theme-form-fields');
+let light_theme_primary_text = document.querySelectorAll('.light-theme-primary-text');
+let light_theme_accent = document.querySelectorAll('.light-theme-accent');
+let header = document.querySelector('.header');
 
-// `;
+let footer = document.querySelector('.footer');
+let light_theme_circle_btn_links = document.querySelectorAll('.light-theme-circle-btn-links');
 
 // ---------- Projects Background Image ---------- //
 
@@ -76,3 +84,102 @@ for (let i = 0; i < small_screen_nav_links.length; i++) {
     });
 
 }
+
+// ---------- Theme Change ---------- //
+
+theme_change_btn.addEventListener('click', function () {
+
+    if (light_theme_indicator.style.display != 'block') {
+        light_theme_indicator.style.display = 'block';
+        console.log('Display Block');
+
+        // ---------- Background Color Changer ---------- //
+
+        header.style.backgroundColor = `#0c0f1d`;
+        footer.style.backgroundColor = `#0c0f1d`;
+
+        // ---------- HTML Elements Color Change ---------- //
+
+        for (let i = 0; i < light_theme_text.length; i++) {
+            light_theme_text[i].classList.remove('light-theme-text');
+            light_theme_text[i].classList.add('dark-theme-text');
+        }
+
+        for (let j = 0; j < light_theme_background.length; j++) {
+            light_theme_background[j].classList.remove('light-theme-background');
+            light_theme_background[j].classList.add('dark-theme-background');
+        }
+
+        for (let j = 0; j < light_theme_primary.length; j++) {
+            light_theme_primary[j].classList.remove('light-theme-primary');
+            light_theme_primary[j].classList.add('dark-theme-primary');
+        }
+
+        for (let j = 0; j < light_theme_form_fields.length; j++) {
+            light_theme_form_fields[j].classList.remove('light-theme-form-fields');
+            light_theme_form_fields[j].classList.add('dark-theme-form-fields');
+        }
+
+        for (let j = 0; j < light_theme_primary_text.length; j++) {
+            light_theme_primary_text[j].classList.remove('light-theme-primary-text');
+            light_theme_primary_text[j].classList.add('dark-theme-primary-text');
+        }
+
+        for (let j = 0; j < light_theme_accent.length; j++) {
+            light_theme_accent[j].classList.remove('light-theme-accent');
+            light_theme_accent[j].classList.add('dark-theme-accent');
+        }
+
+        for (let j = 0; j < light_theme_circle_btn_links.length; j++) {
+            light_theme_circle_btn_links[j].classList.remove('light-theme-circle-btn-links');
+            light_theme_circle_btn_links[j].classList.add('dark-theme-circle-btn-links');
+        }
+    }
+
+    else {
+        light_theme_indicator.style.display = 'none';
+        console.log('Display None');
+
+        // ---------- Background Color Changer ---------- //
+
+        header.style.backgroundColor = `#fff`;
+        footer.style.backgroundColor = `#fff`;
+
+        // ---------- HTML Elements Color Change ---------- //
+
+        for (let i = 0; i < light_theme_text.length; i++) {
+            light_theme_text[i].classList.remove('dark-theme-text');
+            light_theme_text[i].classList.add('light-theme-text');
+        }
+
+        for (let j = 0; j < light_theme_background.length; j++) {
+            light_theme_background[j].classList.remove('dark-theme-background');
+            light_theme_background[j].classList.add('light-theme-background');
+        }
+
+        for (let j = 0; j < light_theme_primary.length; j++) {
+            light_theme_primary[j].classList.remove('dark-theme-primary');
+            light_theme_primary[j].classList.add('light-theme-primary');
+        }
+
+        for (let j = 0; j < light_theme_form_fields.length; j++) {
+            light_theme_form_fields[j].classList.remove('dark-theme-form-fields');
+            light_theme_form_fields[j].classList.add('light-theme-form-fields');
+        }
+
+        for (let j = 0; j < light_theme_primary_text.length; j++) {
+            light_theme_primary_text[j].classList.remove('dark-theme-primary-text');
+            light_theme_primary_text[j].classList.add('light-theme-primary-text');
+        }
+
+        for (let j = 0; j < light_theme_accent.length; j++) {
+            light_theme_accent[j].classList.remove('dark-theme-accent');
+            light_theme_accent[j].classList.add('light-theme-accent');
+        }
+
+        for (let j = 0; j < light_theme_circle_btn_links.length; j++) {
+            light_theme_circle_btn_links[j].classList.remove('dark-theme-circle-btn-links');
+            light_theme_circle_btn_links[j].classList.add('light-theme-circle-btn-links');
+        }
+    }
+});
